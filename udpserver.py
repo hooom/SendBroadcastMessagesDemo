5a4e6996afb03ae9ac7a308a48014a63c8f9a1d8
hooom <houman.march@gmail.com>
2013-05-02 22:51:02 +0800

    add

---
+++ udpserver.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright : (c) SAEKI Yoshiyasu
+# License   : MIT-style license
+#             <http://www.opensource.org/licenses/mit-license.php>
+# last updated: 2012/05/20
+
+import socket
+
+if __name__ == "__main__":
+    udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udpsock.bind(("", 8888))
+    print "start listening:8888"
+    while True:
+        data, addr = udpsock.recvfrom(1024)
+        print "%s:%s wrote:" % addr,
+        print "%s" % data
+        #udpsock.sendto("You wrote: %s" % data, addr)
